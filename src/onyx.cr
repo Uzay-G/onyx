2e3bcc32b78406e42cd666745c2247bc63c0688f
Uzay-G <uzgirit@gmail.com>
2020-05-05 14:57:59 +0200

    adding commands

--- src/onyx.cr
+++ src/onyx.cr
@@ -10,9 +10,10 @@ client = Discord::Client.new(token: CONFIG["discord_token"].to_s, client_id: CON
 
 client.on_message_create do |message|
   author = message.author
-  if message.content.starts_with? "~"
+  commands = message.content.strip.split(" ")
+  if commands[0].starts_with? "~"
     user = nil
-    if message.content == "~pin"
+    if commands[0] == "~pin"
       begin
         user = User.query.find { id == author.id.value }
       rescue ex
@@ -30,13 +31,19 @@ client.on_message_create do |message|
         end
       end
       begin
-        quoted_text = client.get_channel_messages(channel_id: message.channel_id, limit: 2, around: message.id)[1]
         quote = Bookmark.new
-        quote.id = quoted_text.id.value
-        quote.body = quoted_text.content
+        quoted_text = nil
+        if commands.size < 2
+          quoted_text = client.get_channel_messages(channel_id: message.channel_id, limit: 2, around: message.id)[1]
+          quote.id = quoted_text.id.value
+          quote.body = quoted_text.content
+        else
+          quote.id = message.id.value
+          quote.body = commands[1..-1].join(" ")
+        end
         if (user)
           quote.user_id = user.id
-          quote.author_name = quoted_text.author.username
+          quote.author_name = quoted_text.nil? ? user.username : quoted_text.author.username
         end
         if quote.save!
           client.create_message(message.channel_id, "Message saved!")
@@ -44,20 +51,29 @@ client.on_message_create do |message|
       rescue ex
         puts ex.message
       end
-    elsif message.content.starts_with? "~list"
+
+    elsif commands[0] == "~list"
       user = User.query.find { id == author.id.value }
       if (user.nil?)
-        client.create_message(message.channel_id, "Message saved!")
+        client.create_message(message.channel_id, "You don't have any bookmarks")
       else
         quotes = Bookmark.query.where {user_id == author.id.value }
-        quotes.each { |quote| client.create_message(message.channel_id, "From **#{quote.author_name}**\n #{quote.body} \n No: #{quote.id}")}
+        if quotes.nil?
+          client.create_message(message.channel_id, "You don't have any bookmarks")
+        else
+          dm = client.create_dm(recipient_id: author.id.value)
+          client.create_message(message.channel_id, "Bookmarks sent to dms!")
+          quotes.each { |quote| client.create_message(dm.id.value, "From **#{quote.author_name}**\n #{quote.body} \n No: #{quote.id}")}
+        end
       end      
-    elsif message.content.starts_with? "~delete"
+    elsif commands[0] == "~delete"
       begin
-        number = message.content.strip.split(" ")[1].to_i64
+        number = commands[1].to_i64
         bookmark = Bookmark.query.find { id == number }
         if (bookmark.nil?)
           client.create_message(message.channel_id, "Bookmark could not be found")
+        elsif (author.id.value != bookmark.user_id)
+          client.create_message(message.channel_id, "You aren't allowed to delete that bookmark")
         else
           bookmark.delete
           client.create_message(message.channel_id, "Deleted this message from **#{bookmark.author_name}**\n > #{bookmark.body} has been deleted")
@@ -65,6 +81,20 @@ client.on_message_create do |message|
       rescue ex
         puts ex.message
       end
+    elsif commands[0] == "~deleteall"
+      user = User.query.find { id == author.id.value }
+      if (user.nil?)
+        client.create_message(message.channel_id, "You don't have any bookmarks.")
+      else
+        bookmarks = Bookmark.query.where { user_id == user.id }.to_delete.execute
+        client.create_message(message.channel_id, "All bookmarks deleted. :cry:")
+      end
+    elsif commands[0] == "~help"
+      help = "**~pin**, saves the last message to your bookmarks or specify text to save
+              **~delete <id>**, delete bookmark from your collection
+              **~deleteall**, delete your entire collection of bookmarks
+              **~list**, sends all your bookmarks to dms"
+      client.create_message(message.channel_id, help)
     end
   end
 end
